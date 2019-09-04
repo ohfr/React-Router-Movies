29f233d008e2e8efc799ed2f68ad79d40fdeda20
Daniel Martin <danielmartin1205@gmail.com>
2019-09-04 02:50:23 -0400

    added mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 1;
+    const id = parseInt(props.match.params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,6 +13,7 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          
         })
         .catch(error => {
           console.error(error);
@@ -42,12 +43,11 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
+        {/* {stars.map(star => {
+          return (<div key={star} className="movie-star">
             {star}
-          </div>
-        ))}
+          </div>);
+        })} */}
       </div>
       <div className="save-button">Save</div>
     </div>
